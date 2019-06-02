7edbfbbb9add45e0ea99b896549aea7f9243c3ed
Anishdd5 <51048894+Anishdd5@users.noreply.github.com>
2019-06-02 17:24:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1427,7 +1427,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1445,7 +1445,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1464,7 +1464,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1483,7 +1483,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1501,7 +1501,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1519,7 +1519,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1537,7 +1537,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1554,7 +1554,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1571,7 +1571,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1590,7 +1590,7 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -1609,12 +1609,28 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
         footer: {
-          text: "By Abo Khalil"
+          text: "By TitanMc"
         }
       }}).then(msg => {msg.delete(4000)});
                           }
 
      
 });
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('كس اختك')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By TitanMc"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+}); 
  
 client.login(process.env.BOT_TOKEN)
